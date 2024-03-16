9fb6eed47f1bd5bc3dedab12bb376beb5b1a6d41
neil <jacoborosseau@gmail.com>
2024-03-16 17:41:29 -0500

    cleancode-solid

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Proyecto para practicar
+## PRINCIPIOS SOLID-CLEAN CODE
 
-Este es un proyecto de Vanilla TypeScript en Vite, para trabajar los ejercicios del curso sobre Principios SOLID y CleanCode.
+
 
 Clonar o descargar el proyecto y luego:
 
---
+++ src/clean-code/02-name.ts
@@ -0,0 +1,54 @@
+(() => {
+
+
+    const celciusTemperature = [33.6, 12.34];
+
+
+    const serverIp = '123.123.123.123';
+
+    const userList = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    // Listado de emails de los usuarios
+    const usersEmail = userList.map( user => user.email );
+
+    // Variables booleanas de un video juego
+    const canJump = false;
+    const canRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+
+    const startTime = new Date().getTime();
+
+    const endTime = new Date().getTime() - startTime;
+
+
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+
+    function getBooksByUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+
+    function getSquareArea( s: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
+
+
+
+
